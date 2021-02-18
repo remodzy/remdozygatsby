62ec5181fa5300af621330289cd62b73dacaf970
Alexey Lukin <alexey.lukin@da-14.com>
2021-02-18 20:19:36 +0200

    remove scroll rataining

---
+++ gatsby-browser.js
@@ -0,0 +1,14 @@
+exports.shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition,
+}) => {
+  const { pathname } = location
+  // list of routes for the scroll-to-top-hook
+  const scrollToTopRoutes = [`/privacy`, `/terms`]
+  // if the new route is part of the list above, scroll to top (0, 0)
+  if (scrollToTopRoutes.indexOf(pathname) !== -1) {
+    window.scrollTo(0, 0)
+  }
+
+  return false
+}
