260a5c358ac86877a1a799eae8682d593c59711a
Alexey Lukin <alexey.lukin@da-14.com>
2021-03-19 21:00:11 +0200

    add blog to landing page

--- src/templates/Landing/index.tsx
+++ src/templates/Landing/index.tsx
@@ -2,11 +2,13 @@ import React, { FC, ReactElement } from 'react'
 
 import PrimaryBlock from './components/PrimaryBlock'
 import ProductsBlock from './components/ProductsBlock'
+import RBlog from '~components/RBlog'
 
 const LandingTemplate: FC<unknown> = (): ReactElement => (
   <>
     <PrimaryBlock />
     <ProductsBlock />
+    <RBlog />
   </>
 )
 
