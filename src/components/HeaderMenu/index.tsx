9f825a7dc7e6f28062c6f37620b70bba76ad588e
Alexey Lukin <alexey.lukin@da-14.com>
2021-09-16 23:41:27 +0300

    replace header menu from database

--- src/components/HeaderMenu/index.tsx
+++ src/components/HeaderMenu/index.tsx
@@ -1,63 +1,43 @@
 import { useLocation } from '@reach/router'
-import { Link } from 'gatsby'
+import { graphql, Link, useStaticQuery } from 'gatsby'
 import React, { ReactElement, useCallback, useState } from 'react'
 import { isMobile } from 'react-device-detect'
 
 import ModalContainer from '~components/ModalContainer'
+import { isSSR } from '~utils/hooks'
 
 import DesktopProductsMenu from './DesktopProductsMenu'
 import FloatMenu from './FloatMenu'
 import * as styles from './HeaderMenu.module.css'
 import MenuToggle from './MenuToggle'
-import { isSSR } from '~utils/hooks'
 
 export type MenuItem = {
   link: string
   text: string
+  isButton: boolean
+  isLinkButton: boolean
 }
 
-const homeMenuList: MenuItem[] = [
-  {
-    link: '/support',
-    text: 'Support',
-  },
-  {
-    link: '//docs.roxabo.com',
-    text: 'Docs',
-  },
-  {
-    link: '/blog',
-    text: 'Blog',
-  },
-]
-
-const productMenuList: MenuItem[] = [
-  {
-    link: '/support',
-    text: 'Support',
-  },
-  {
-    link: '//docs.roxabo.com',
-    text: 'Docs',
-  },
-  {
-    link: '/pricing',
-    text: 'Pricing',
-  },
-  {
-    link: '/blog',
-    text: 'Blog',
-  },
-]
+export type ContentfulMenuItem = {
+  id: string
+  order: number
+  name: string
+  url: string
+  isButton: boolean
+  isLinkButton: boolean
+  showAt: Array<string>
+}
 
 export default function HeaderMenu(): ReactElement {
   const [show, setShow] = useState(false)
 
   const { pathname } = useLocation()
 
-  let menuList = homeMenuList
-  if (['/roxservice/', '/roxforms/', '/roxflow/'].indexOf(pathname) !== -1)
-    menuList = productMenuList
+  const items = useStaticQuery(query)
+  const menuItems = prepareHeaderItems(
+    items.allContentfulHeaderMenu.nodes,
+    pathname
+  )
 
   const handleClick = useCallback(() => {
     setShow(!show)
@@ -71,7 +51,7 @@ export default function HeaderMenu(): ReactElement {
         <MenuToggle show={false} handleClick={handleClick} />
         {show && (
           <ModalContainer>
-            <FloatMenu menuList={menuList} handleClose={handleClick} />
+            <FloatMenu menuList={menuItems} handleClose={handleClick} />
           </ModalContainer>
         )}
       </>
@@ -81,7 +61,7 @@ export default function HeaderMenu(): ReactElement {
   return (
     <div className={styles.root}>
       <DesktopProductsMenu />
-      {menuList.map(item =>
+      {menuItems.map(item =>
         /^\/(?!\/)/.test(item.link) ? (
           <Link key={item.text} className={styles.link} to={item.link}>
             {item.text}
@@ -101,3 +81,38 @@ export default function HeaderMenu(): ReactElement {
     </div>
   )
 }
+
+const prepareHeaderItems = (
+  items: Array<ContentfulMenuItem>,
+  pathname: string
+): Array<MenuItem> => {
+  const menuItems: Array<MenuItem> = []
+  items
+    .sort((a, b) => a.order - b.order)
+    .forEach(item => {
+      if (!item.showAt || item.showAt.indexOf(pathname) !== -1)
+        menuItems.push({
+          link: item.url,
+          text: item.name,
+          isButton: item.isButton,
+          isLinkButton: item.isLinkButton,
+        })
+    })
+  return menuItems
+}
+
+export const query = graphql`
+  query allContentfulHeaderMenuQuery {
+    allContentfulHeaderMenu {
+      nodes {
+        id
+        order
+        name
+        url
+        isButton
+        isLinkButton
+        showAt
+      }
+    }
+  }
+`
